e1463bc78e471487ed7e27d107701a1fe9b6c40d
pardessuccess <zkwhdgur238@gmail.com>
2025-03-03 17:03:18 +0900

    Refactor 1. 필요없는 코드들 제거

--- app/src/main/java/com/pardess/musicplayer/domain/model/enums/PlayerState.kt
+++
@@ -1,8 +0,0 @@
-package com.pardess.musicplayer.domain.model.enums
-
-enum class PlayerState {
-    PLAYING,
-    PAUSED,
-    STOPPED,
-    LOADING
-}
--- app/src/main/java/com/pardess/musicplayer/utils/ControllerExt.kt
+++
@@ -1,9 +0,0 @@
-package com.pardess.musicplayer.utils
-
-import com.pardess.musicplayer.domain.model.enums.PlayerState
-import com.pardess.musicplayer.domain.service.MusicController
-
-
-fun MusicController.isPlaying(): Boolean {
-    return this.getPlayerState() == PlayerState.PLAYING
-}
